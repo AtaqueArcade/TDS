e483bbf013315aee8062a86182dfdd0f805ee509
Incorpo <salvador.ruizl@um.es>
2019-12-02 16:41:43 +0100

    Primer commit, subiendo el proyecto vacio

---
+++ AppChat/src/main/java/TDS/AppChat/App.java
@@ -0,0 +1,13 @@
+package TDS.AppChat;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
