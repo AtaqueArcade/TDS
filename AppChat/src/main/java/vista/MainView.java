2288fe68947e1ccbb087a6af7c754dfb60739d6f
AtaqueArcade <ataquearcade@gmail.com>
2020-02-25 11:11:50 +0100

    Avances en la vista principal

--- AppChat/src/main/java/vista/MainView.java
+++ AppChat/src/main/java/vista/MainView.java
@@ -4,10 +4,20 @@ package vista;
 import javax.swing.JPanel;
 
 import controlador.Controlador;
+import java.awt.BorderLayout;
 
 @SuppressWarnings("serial")
 public class MainView extends JPanel {
 
 	public MainView() {
+		setLayout(new BorderLayout(0, 0));
+
+		JPanel sidebar = new JPanel();
+		add(sidebar, BorderLayout.WEST);
+
+		JPanel toolbar = new JPanel();
+		add(toolbar, BorderLayout.NORTH);
+
+		add(new ChatView(), BorderLayout.CENTER);
 	}
 }
