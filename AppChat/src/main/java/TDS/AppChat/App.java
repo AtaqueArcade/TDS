714ee2da17dd34e7e9f55e4d1047daeb926d4281
AtaqueArcade <ataquearcade@gmail.com>
2020-04-18 16:10:53 +0200

    Interfaz de premium y cambios en la vista de edición de grupos

--- AppChat/src/main/java/TDS/AppChat/App.java
+++ AppChat/src/main/java/TDS/AppChat/App.java
@@ -1,13 +1,14 @@
 package TDS.AppChat;
+
 import vista.Ventana;
 
 public class App {
-	//Open the program
+	// Open the program
 	public static void main(String[] args) {
 		try {
 			Ventana window = new Ventana();
 		} catch (Exception e) {
 			e.printStackTrace();
-		} 
+		}
 	}
 }
--- AppChat/src/main/java/vista/ToolBarView.java
+++ AppChat/src/main/java/vista/ToolBarView.java
@@ -200,7 +200,7 @@ public class ToolBarView extends JPanel {
 
 		JButton btnGroupSettings = new JButton("Group Settings");
 		btnGroupSettings.addActionListener(ev -> {
-			ContactSettingsView();
+			GroupSettingsView();
 		});
 		btnGroupSettings.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnGroupSettings.setPreferredSize(new Dimension(140, 30));
@@ -225,6 +225,10 @@ public class ToolBarView extends JPanel {
 		toolbarMenuView.add(verticalStrut_3);
 
 		JButton btnPremium = new JButton("Upgrade to Premium");
+		btnPremium.addActionListener(ev -> {
+			premiumView();
+		});
+
 		btnPremium.setAlignmentX(Component.CENTER_ALIGNMENT);
 		btnPremium.setPreferredSize(new Dimension(140, 30));
 		btnPremium.setMinimumSize(new Dimension(140, 30));
@@ -381,6 +385,276 @@ public class ToolBarView extends JPanel {
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.pack();
 		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	private void GroupSettingsView() {
+		JTextField textFieldGroupName;
+		JTextField textField;
+		JFrame frame = new JFrame("Group settings");
+		frame.setPreferredSize(new Dimension(400, 300));
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		JPanel panel = new JPanel();
+		JTabbedPane tabbedPane = new JTabbedPane();
+		tabbedPane.addTab("New group", null, panel, "Create a new group chat");
+		panel.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel_1 = new JPanel();
+		panel.add(panel_1, BorderLayout.SOUTH);
+
+		JButton btnConfirm = new JButton("Confirm");
+		btnConfirm.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_1.add(btnConfirm);
+
+		JButton btnCancel = new JButton("Cancel");
+		btnCancel.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		panel_1.add(btnCancel);
+
+		JList list = new JList();
+		list.setModel(new AbstractListModel() {
+			String[] values = new String[] { "ejemplo_1", "ejemplo_2" };
+
+			public int getSize() {
+				return values.length;
+			}
+
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		JPanel panel_5 = new JPanel();
+		panel.add(panel_5, BorderLayout.WEST);
+
+		Component horizontalStrut = Box.createHorizontalStrut(20);
+		panel_5.add(horizontalStrut);
+
+		JScrollPane scrollPane = new JScrollPane(list);
+		panel_5.add(scrollPane);
+
+		JList list_2 = new JList();
+		list_2.setModel(new AbstractListModel() {
+			String[] values = new String[] { "ejemplo_anadido" };
+
+			public int getSize() {
+				return values.length;
+			}
+
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		JPanel scrollPane_2 = new JPanel();
+		panel.add(scrollPane_2, BorderLayout.EAST);
+
+		JScrollPane scrollPane_3 = new JScrollPane(list_2);
+		scrollPane_2.add(scrollPane_3);
+
+		Component horizontalStrut_1 = Box.createHorizontalStrut(20);
+		scrollPane_2.add(horizontalStrut_1);
+
+		JPanel panel_2 = new JPanel();
+		panel_2.setLayout(new BoxLayout(panel_2, BoxLayout.Y_AXIS));
+		panel.add(panel_2, BorderLayout.CENTER);
+
+		JButton btnNewButton_2 = new JButton("-->");
+		btnNewButton_2.setAlignmentX(Component.CENTER_ALIGNMENT);
+		btnNewButton_2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+
+		Component verticalStrut = Box.createVerticalStrut(20);
+		panel_2.add(verticalStrut);
+
+		textFieldGroupName = new JTextField();
+		textFieldGroupName.setToolTipText("Enter the group's name here");
+		panel_2.add(textFieldGroupName);
+		textFieldGroupName.setColumns(10);
+		textFieldGroupName.setPreferredSize(new Dimension(100, 20));
+		textFieldGroupName.setMinimumSize(new Dimension(100, 20));
+		textFieldGroupName.setMaximumSize(new Dimension(100, 20));
+		panel_2.add(btnNewButton_2);
+
+		JButton btnNewButton_3 = new JButton("<--");
+		btnNewButton_3.setAlignmentX(Component.CENTER_ALIGNMENT);
+		panel_2.add(btnNewButton_3);
+
+		JPanel panel2 = new JPanel();
+		tabbedPane.addTab("Group manager", null, panel2, "Edit or delete existing groups");
+		panel2.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel_3 = new JPanel();
+		panel2.add(panel_3, BorderLayout.SOUTH);
+
+		JButton btnConfirm2 = new JButton("Confirm");
+		btnConfirm2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel_3.add(btnConfirm2);
+
+		JButton btnCancel2 = new JButton("Cancel");
+		btnCancel2.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		panel_3.add(btnCancel2);
+
+		JList list2 = new JList();
+		list2.setModel(new AbstractListModel() {
+			String[] values = new String[] { "Grupo_1", "Grupo_2", "Grupo_3" };
+
+			public int getSize() {
+				return values.length;
+			}
+
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		JPanel panel_6 = new JPanel();
+		panel2.add(panel_6, BorderLayout.WEST);
+
+		Component horizontalStrut1 = Box.createHorizontalStrut(20);
+		panel_6.add(horizontalStrut1);
+
+		JScrollPane scrollPane1 = new JScrollPane(list2);
+		panel_6.add(scrollPane1);
+
+		JList list3 = new JList();
+		list3.setModel(new AbstractListModel() {
+			String[] values = new String[] { "contacto del grupo", "1", "2", "3", "4" };
+
+			public int getSize() {
+				return values.length;
+			}
+
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+		JPanel panel1 = new JPanel();
+		panel2.add(panel1, BorderLayout.EAST);
+
+		JScrollPane scrollPane_ = new JScrollPane(list3);
+		panel1.add(scrollPane_);
+
+		Component horizontalStrut_ = Box.createHorizontalStrut(20);
+		panel1.add(horizontalStrut_);
+
+		JPanel panel4 = new JPanel();
+		panel2.add(panel4, BorderLayout.CENTER);
+		panel4.setLayout(new BorderLayout(0, 0));
+
+		JPanel panel_4 = new JPanel();
+		panel4.add(panel_4, BorderLayout.SOUTH);
+
+		JButton btnAdd = new JButton("Add\r\n");
+		panel_4.add(btnAdd);
+
+		JButton btnNewButton_1 = new JButton("Remove\r\n");
+		panel_4.add(btnNewButton_1);
+
+		JPanel panel_7 = new JPanel();
+		panel4.add(panel_7, BorderLayout.CENTER);
+
+		JList contacts = new JList();
+		contacts.setModel(new AbstractListModel() {
+			String[] values = new String[] { "contacto 1", "" };
+
+			public int getSize() {
+				return values.length;
+			}
+
+			public Object getElementAt(int index) {
+				return values[index];
+			}
+		});
+
+		Component horizontalStrut_3 = Box.createHorizontalStrut(20);
+		panel_7.add(horizontalStrut_3);
+		JScrollPane scrollPane_1 = new JScrollPane(contacts);
+		panel_7.add(scrollPane_1);
+
+		Component horizontalStrut_2 = Box.createHorizontalStrut(20);
+		panel_7.add(horizontalStrut_2);
+
+		textField = new JTextField();
+		textField.setColumns(10);
+		textField.setPreferredSize(new Dimension(100, 20));
+		textField.setMinimumSize(new Dimension(100, 20));
+		textField.setMaximumSize(new Dimension(100, 20));
+		panel4.add(textField, BorderLayout.NORTH);
+		JPanel l3 = new JPanel();
+		panel2.add(l3, BorderLayout.NORTH);
+
+		JLabel lblNewLabel = new JLabel("Select the group");
+		lblNewLabel.setHorizontalAlignment(SwingConstants.LEFT);
+		l3.add(lblNewLabel);
+
+		Component horizontalStrut_4 = Box.createHorizontalStrut(40);
+		l3.add(horizontalStrut_4);
+
+		JLabel lblNewLabel_1 = new JLabel("Your contacts");
+		lblNewLabel_1.setHorizontalAlignment(SwingConstants.CENTER);
+		l3.add(lblNewLabel_1);
+
+		Component horizontalStrut_5 = Box.createHorizontalStrut(50);
+		l3.add(horizontalStrut_5);
+
+		JLabel lblNewLabel_2 = new JLabel("Contacts inside group");
+		lblNewLabel_2.setHorizontalAlignment(SwingConstants.RIGHT);
+		l3.add(lblNewLabel_2);
+
+		frame.getContentPane().add(tabbedPane);
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.setVisible(true);
+	}
+
+	private void premiumView() {
+		JFrame frame = new JFrame("Go premium");
+		frame.setPreferredSize(new Dimension(400, 300));
+		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+		frame.pack();
+		frame.setLocationRelativeTo(null);
+		frame.getContentPane().setLayout(new BorderLayout(0, 0));
+
+		JPanel panel = new JPanel();
+		frame.getContentPane().add(panel, BorderLayout.SOUTH);
+
+		JButton btnPremium = new JButton("Go Premium");
+		btnPremium.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+			}
+		});
+		panel.add(btnPremium);
+
+		JButton btnBack = new JButton("Back");
+		btnBack.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				frame.dispose();
+			}
+		});
+		panel.add(btnBack);
+
+		ImageIcon imageIcon = new ImageIcon("C:/Users/Dani/git/TDS/AppChat/resources/Singtel-Music-8-Mar-2017.jpg");
+		Image image = imageIcon.getImage();
+		Image newimg = image.getScaledInstance(380, 220, java.awt.Image.SCALE_SMOOTH);
+		imageIcon = new ImageIcon(newimg);
+
+		JPanel panel_1 = new JPanel();
+		panel_1.add(new JLabel(imageIcon));
+		frame.getContentPane().add(panel_1, BorderLayout.CENTER);
 		frame.setVisible(true);
 	}
 }
